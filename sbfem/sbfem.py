bf91c25215918e0939808bb38c24700854291a3f
Arsen <arsen.memetov@aspose.com>
2020-02-11 17:28:06 +0200

    Implemented method sbfem, SElementSlnEigenMethod in Matlab.

--- sbfem/sbfem.py
+++ sbfem/sbfem.py
@@ -62,7 +62,7 @@ def coeffMatricesOfSElement(xy, conn, mat: Material):
                     The nodes are numbered locally within an S-element starting from 0
     :param conn[ie, :]: local connectivity matrix of line element ie in the local nodal numbers of an S-element
     :param mat: material constants (elasticity matrix, mass density)
-    :return: e0, e1, e2, m0  - coefficient matrices of S-element
+    :return: E0, E1, E2, M0 - coefficient matrices of S-element
     """
 
     # number of DOFs at boundary (2 DOFs per node)
@@ -95,3 +95,80 @@ def coeffMatricesOfSElement(xy, conn, mat: Material):
         M0[d[:, np.newaxis], d] += em0
 
     return E0, E1, E2, M0
+
+
+def sbfem(E0, E1, E2, M0):
+    """
+    Original name: SElementSlnEigenMethod (p.79)
+    :param E0: coefficient matrix of S-Element
+    :param E1: coefficient matrix of S-Element
+    :param E2: coefficient matrix of S-Element
+    :param M0: coefficient matrix of S-Element
+    :return: a tuple (K, d, v11, M) where
+    K   - stiffness matrix of S-Element
+    d   - eigenvalues
+    v11 - upper half of eigenvectors (displacement modes)
+    M   - mass matrix of S-Element
+    """
+
+    # number of DOFs of boundary nodes
+    nd = E0.shape[0]
+
+    # Preconditioning
+    # Eq.(3.28)
+    Pf = 1. / np.sqrt(np.abs(np.diag(E0)))
+    P = np.diag(Pf)
+    # Eq.(3.27)
+    E0 = P.dot(E0).dot(P)
+    E1 = P.dot(E1).dot(P)
+    E2 = P.dot(E2).dot(P)
+
+    # Construct Zp - Eq.(3.30)
+    E0_inv = np.linalg.inv(E0)
+    m1 = np.concatenate((-E0_inv.dot(E1.T), E0_inv), axis=1)
+    m2 = np.concatenate((E2 + E1.dot(m1[:, :nd]), -m1[:, :nd].T), axis=1)
+    Zp = np.concatenate((m1, m2))
+
+    # eigenvalues and eigenvectors - Eq.(3.29)
+    d, v = np.linalg.eig(Zp)
+
+    # index for sorting eignvalues in descending order of real part.
+    idx = np.argsort(np.real(d))[::-1]
+
+    # select eigenvalues and eigenvectors for solution of bounded domain
+    # select the first half of sorted eigenvalues
+    d = d[idx[:nd]]
+    # select the corresponding eigenvectors
+    v = v[:, idx[:nd]]
+    v = np.diag(np.concatenate((Pf, 1./Pf), axis=0)).dot(v) # Eq.(3.5)
+
+    # modes of translational rigid body motion, see Item 2 on page 75
+    # set last two eigenvalues to zero
+    d[-2:] = 0
+    # set last two eigenvectors to zero
+    v[:, -2:] = 0
+    # set u_{x}=1 in {phi^(u)_{n-1}}
+    v[0:nd:2, -2] = 1
+    # set u_{y}=1 in {phi^(u)_{n}}
+    v[1:nd:2, -1] = 1
+
+    # normalization of eigenvectors - Eq.(3.37)
+    v = v / np.max(np.abs(v[0:nd-2, :]), axis=0)
+    v11 = v[0:nd, :]
+    v11inv = np.linalg.inv(v11)
+
+    # stiffness matrix - Eq. (3.25)
+    K = np.real(v[nd:, :].dot(v11inv))
+
+    # mass matrix
+    M0 = v11.T.dot(M0).dot(v11) # Eq. (3.103)
+    # am is a square matrix with all columns being the vector of eigenvalues
+    # The entry (i, j) equals λ_i.
+    # The entry (i, j) of am' equals λ_j
+    am = np.tile(np.expand_dims(d, 1), (1, nd))
+    # the entry (i, j) of am+am' equals to λ_i + λ_j
+    M0 = M0 / (2+am+am.T)
+    # Eq. (3.105)
+    M = np.real(v11inv.T.dot(M0).dot(v11inv))
+
+    return K, d, v, M
